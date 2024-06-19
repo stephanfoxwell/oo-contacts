f32be9b0550c6602bef480daabe256a2a26f765d
Stephan Foxwell <stephanfoxwell@icloud.com>
2024-06-19 13:12:17 -0400

    .

--- pages/login.js
+++ pages/login.js
@@ -1,103 +1,99 @@
 // pages/login.js
-import { useState, useEffect } from 'react';
-
+import { useState } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { ButtonPrimary, ButtonText } from '../components/Button/index'
+import TextField from '../components/ui/TextField'
 
 import { directus } from '../middlewares/directusClient';
 
-export default function Login() {
+const Login = () => {
 
   const router = useRouter();
+  const [error, setError] = useState(undefined);
 
-  const [email, setEmail] = useState('stephan@theoutlawocean.com');
-  const [password, setPassword] = useState('rrzDwYQNmp');
-  const [otp, setOtp] = useState('');
-  const [error, setError] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const [userToken, setUserToken] = useState(null);
-
-  const handleLogin = async () => {
-
-    const response = await fetch('/api/auth', {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const body = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+      otp: e.currentTarget.otp.value
+    };
+    const res = await fetch('/api/auth', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password, otp })
+      body: JSON.stringify(body),
     });
-
-    if (response.ok) {
-      // Handle successful login
-      console.log('Login successful');
-      const result = await response.json();
-      console.log(result)
-      setIsAuthenticated(true);
-      setUserToken(result.access_token);
+    if (res.status === 200) {
       router.push('/contacts');
     } else {
-      const errorData = await response.json();
-      setError(errorData.message);
-      setIsAuthenticated(false);
+      setError('Incorrect username or password. Please try again.');
     }
-  };
-
-  const testIsAuthenticated = async () => {
-    const response = await fetch('/api/auth/me', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include'
-    })
-
-    if (response.ok) {
-      // Handle successful login
-      const result = await response.json();
-      console.log(result);
-      setIsAuthenticated(true);
-    }
-    else {
-      const errorData = await response.json();
-      console.log(errorData);
-    }
-  };
-
-  useEffect(() => {
-    testIsAuthenticated();
-  },[])
-
-
-  if ( isAuthenticated ) {
-    return (
-      <div>
-        {userToken && <p>Token: {userToken}</p>}
-        <h1>Logged in</h1>
-        <button onClick={() => setIsAuthenticated(false)}>Logout</button>
-      </div>
-    )
   }
-  else {
-    return (
-      <div>
-        <h1>Login</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="OTP Code"
-          value={otp}
-          onChange={(e) => setOtp(e.target.value)}
-        />
-        <button onClick={handleLogin}>Login</button>
-        {error && <p>{error}</p>}
-      </div>
-    );
+
+  return (
+    <StyledLoginWrap>
+      <StyledLogin>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <TextField
+            id="email"
+            type="email"
+            name="email"
+            autoFocus="true"
+            value="stephan@theoutlawocean.com"
+          />
+          <label htmlFor="password">Password</label>
+          <TextField
+            id="password"
+            type="password"
+            name="password"
+            value="rrzDwYQNmp"
+          />
+          <label htmlFor="otp">Two-Factor Verification Code</label>
+          <TextField
+            id="otp"
+            type="text"
+            name="otp"
+          />
+          <ButtonPrimary type="submit">Sign in</ButtonPrimary>
+          {error && <p>{error}</p>}
+          <ButtonText as={Link} href="/forget-password">Forgot password</ButtonText>
+        </form>
+      </StyledLogin>
+    </StyledLoginWrap>
+  );
+};
+
+export default Login;
+
+
+const StyledLoginWrap = styled.div`
+  min-height: calc(100vh - var(--height-nav));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const StyledLogin = styled.div`
+  position: relative;
+  max-width: 16em;
+  width: 100%;
+  label {
+    font-size: 0.875em;
+    font-weight: 500;
+    margin: 1em 0 0.25em;
   }
-}
+  button {
+    margin: 1em 0;
+  }
+  a {
+    font-size: 0.75em;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  button {
+    margin-right: 1em;
+  }
+`
